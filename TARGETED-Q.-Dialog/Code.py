3d174a826e4fe7143a5ba34005574b9a551b8f6c
kiubb02 <71399699+kiubb02@users.noreply.github.com>
2021-04-14 08:27:41 +0200

    Update Code.py

--- TARGETED-Q.-Dialog/Code.py
+++ TARGETED-Q.-Dialog/Code.py
@@ -1,10 +1,15 @@
-def _common_testing_show_targeted_question_dialog(): #Funktionsname => so machen dass das auf bedingung reagiert
+# TARGETED QUESTION DIALOG
+# EXAMPLE CODE AND EXPLANATION BELOW
+# base :
+# Class for our Dialogue-Window
+class CommonTargetedQuestionDialog(question_text, question_tokens=(), ok_text_identifier=CommonStringId.OK, ok_text_tokens=(), cancel_text_identifier=CommonStringId.CANCEL, cancel_text_tokens=(), mod_identity=None)
+
+def _common_testing_show_targeted_question_dialog():
 
     # define two types of Dialogs
 
-    # Alle UI properties sind im ui_dialog.py zu finden
-
-    def _ok_chosen(_: UiDialogOkCancel): # mit hilfe des UI Handlings
+    # All UI Properties are found in ui_dialog.py
+    def _ok_chosen(_: UiDialogOkCancel):
         pass
 
     def _cancel_chosen(_: UiDialogOkCancel):
@@ -12,14 +17,17 @@ def _common_testing_show_targeted_question_dialog(): #Funktionsname => so machen
 
     # LocalizedStrings within other LocalizedStrings
     description_tokens = (CommonLocalizationUtils.create_localized_string(CommonStringId.TESTING_TEST_TEXT_WITH_SIM_FIRST_AND_LAST_NAME, tokens=(CommonSimUtils.get_active_sim_info(),), text_color=CommonLocalizedStringColor.BLUE),)
-    # Definiere welchen Dialog wir haben möchten
+    # Define the Dialogue
+    # => Call the Class
     dialog = CommonTargetedQuestionDialog(
+        # Fill the properties of that specific Class
         CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
-        # question_tokens (Iterator[Any], optional) – Tokens to format into the question text.
+        question_text = "Wanna go shopping ?",
+        # question_tokens  – Tokens to format into the question text.
         question_tokens=description_tokens,
-        # ok_text_identifier (Union[int, str, LocalizedString, CommonStringId], optional) – A decimal identifier for the Ok text.
+        # ok_text_identifier  – A decimal identifier for the Ok text.
         ok_text_identifier=CommonLocalizationUtils.create_localized_string(CommonStringId.TESTING_TEST_BUTTON_ONE, text_color=CommonLocalizedStringColor.RED),
-        # cancel_text_identifier (Union[int, str, LocalizedString, CommonStringId], optional) – A decimal identifier for the Cancel text.
+        # cancel_text_identifier  – A decimal identifier for the Cancel text.
         cancel_text_identifier=CommonStringId.TESTING_TEST_BUTTON_TWO
     )
     dialog.show(
@@ -28,8 +36,8 @@ def _common_testing_show_targeted_question_dialog(): #Funktionsname => so machen
         tuple(CommonSimUtils.get_sim_info_for_all_sims_generator())[0],
 
         # 2 Options of buttons in that Dialogue Windows :
-        # on_ok_selected (Callable[[UiDialogOkCancel], Any], optional) – Invoked upon the player clicking the Ok button in the dialog.
+        # on_ok_selected – Invoked upon the player clicking the Ok button in the dialog.
         on_ok_selected=_ok_chosen,
-        # on_cancel_selected (Callable[[UiDialogOkCancel], Any], optional) – Invoked upon the player clicking the Cancel button in the dialog.
+        # on_cancel_selected – Invoked upon the player clicking the Cancel button in the dialog.
         on_cancel_selected=_cancel_chosen
     )
