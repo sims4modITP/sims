5ed57293e8dea408cb29201b21fde2221c4bd8cd
inaschmina <80634958+inaschmina@users.noreply.github.com>
2021-04-29 13:06:42 +0200

    Update ok-dialog.py

--- OK-Dialog/ok-dialog.py
+++ OK-Dialog/ok-dialog.py
@@ -37,3 +37,36 @@ def _ok(_connection: int = None):
         CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to show dialog', exception=ex) #falls error -> log
         output('Failed to show ok dialog, please locate your exception log file.')
     output('Done showing.')
+
+    
+    @sims4.commands.Command('start', command_type=sims4.commands.CommandType.Live)
+def _ok(_connection: int = None):
+    output = sims4.commands.CheatOutput(_connection)
+    output('Showing test ok dialog.')
+
+    def _on_acknowledged(_dialog: UiDialogOk):
+        if _dialog.accepted:
+            output('Ok option chosen.')
+        else:
+            output('Dialog closed.')
+
+    try:
+        # LocalizedStrings within other LocalizedStrings
+        title_tokens = (CommonLocalizationUtils.create_localized_string('Story Headline', text_color=CommonLocalizedStringColor.GREEN),)
+        description_tokens = (CommonLocalizationUtils.create_localized_string('Actual Story', tokens=(CommonSimUtils.get_active_sim_info(),), text_color=CommonLocalizedStringColor.BLUE),)
+        dialog = CommonOkDialog(
+            'This is you!', # this is where the headline is written -> insert headline here
+            'You wanted to be a computer scientist your whole life and now, finally, that dream is coming true! '
+            'You just received the Letter – you are an employee at Wayfair starting Monday morning. '
+            'But let’s be honest you sent too many letters of application to even remember what kind of company Wayfair'
+            ' is, right? Let’s check it out!', # this is where the body part is written -> insert tet here
+            title_tokens=title_tokens,
+            description_tokens=description_tokens,
+            ok_text_identifier=CommonLocalizationUtils.create_localized_string('Lets go!', text_color=CommonLocalizedStringColor.RED)
+        )
+        dialog.show(on_acknowledged=_on_acknowledged)
+    except Exception as ex:
+        CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to show dialog', exception=ex)
+        output('Failed to show ok dialog, please locate your exception log file.')
+    output('Done showing.')
+    
