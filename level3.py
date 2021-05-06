e3f02d51f28b365ae9f79274c2713b97d2dc774c
kiubb02 <71399699+kiubb02@users.noreply.github.com>
2021-05-06 12:31:19 +0200

    Update level3.py

--- level3.py
+++ level3.py
@@ -1,3 +1,14 @@
+# alle imports für einen ok - dialog
+import sims4.commands
+
+from sims4communitylib.enums.strings_enum import CommonStringId
+from sims4communitylib.exceptions.common_exceptions_handler import CommonExceptionHandler
+from sims4communitylib.modinfo import ModInfo
+from sims4communitylib.utils.localization.common_localized_string_colors import CommonLocalizedStringColor
+from sims4communitylib.utils.localization.common_localization_utils import CommonLocalizationUtils
+from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
+from ui.ui_dialog import UiDialogOk
+from sims4communitylib.dialogs.common_ok_dialog import CommonOkDialog #die common_ok_dialog klasse wird importiert...
 # alle Dialogue für die dritte Stufe
 
 
