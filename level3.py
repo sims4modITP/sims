46d0f2f68ed63a2251fc28dc8a005fc8526e5292
Miriaana <80634835+Miriaana@users.noreply.github.com>
2021-05-07 16:24:35 +0200

    Update level3.py

--- level3.py
+++ level3.py
@@ -56,6 +56,53 @@ def _ok(_connection: int = None):
 
 
 # 3 Ok Dialog => Miriam
+@sims4.commands.Command('three_third_dialogue', command_type=sims4.commands.CommandType.Live)
+def three_third_dialogue(_connection: int = None):
+    output = sims4.commands.CheatOutput(_connection)
+    output('Showing testxyz ok dialog.')
 
+    def _on_acknowledged(
+            _dialog: UiDialogOk):  # console logs für den rückgabewert des dialoges -> wurde auf ok gedrückt oder einfach geschlossen?
+        if _dialog.accepted:
+            output('Ok option chosen.')
+        else:
+            output('Dialog closed.')
+
+    try:
+        title_tokens = (CommonLocalizationUtils.create_localized_string('Story Headline',
+                                                                        text_color=CommonLocalizedStringColor.GREEN),)
+        description_tokens = (
+            CommonLocalizationUtils.create_localized_string('Actual Story',
+                                                            tokens=(CommonSimUtils.get_active_sim_info(),),
+                                                            text_color=CommonLocalizedStringColor.BLUE),)
+        dialog = CommonOkDialog(
+            'Charlotte Brandt White/Navy Hallway Runner',
+            # this is where the headline is written -> insert headline here
+            '11.03.2021 \n\n'
+            'Complement your coastal - inspired decor with this breezy indoor / outdoor area rug, featuring a simple quatrefoil design. '
+            'It\'s flatwoven in Turkey from water- and fade-resistant polypropylene, so it can easily stand up to tough stains and spills. '
+            'The backing is safe to roll out in rooms with floor heating. '
+            'And the soft navy blue and white hues work well with any color palette you can dream up. '
+            'Plus, it has a low 0.09" pile height that\'s ideal for spaces with high foot traffic like the entryway or '
+            'patio, and for homes with little ones and furry friends running around.\n\n'
+            'Details:\n\n'
+            'Name: Charlotte Brandt White/Navy Hallway Runner\n\n'
+            'Size: Rectangle 5’3’’ x 7’3’’\n\n'
+            'Overall Product Weight: 34.0 lb.\n\n'
+            'Free Shipping on orders over $35.00',
+
+            # this is where the body part is written -> insert tet here
+            title_tokens=title_tokens,
+            description_tokens=description_tokens,
+            ok_text_identifier=CommonLocalizationUtils.create_localized_string('Close',
+                                                                               text_color=CommonLocalizedStringColor.GREEN)
+        )
+        # dialog.show(IconInfoData(CommonResourceUtils.get_resource_key(Types.PNG, 7169552662410693051)))
+        dialog.show()
+    except Exception as ex:
+        CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to show dialog',
+                                             exception=ex)  # falls error -> log
+        output('Failed to show ok dialog, please locate your exception log file.')
+    output('Done showing.')
 
 # Notification => freie Wahl
