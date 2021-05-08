f09ba477469700a83793439adf53af80698791c8
cruzifixu <80634953+cruzifixu@users.noreply.github.com>
2021-05-08 12:28:42 +0200

    Update level1_2.py

--- level1_2.py
+++ level1_2.py
@@ -13,6 +13,69 @@ from sims4communitylib.dialogs.common_ok_dialog import CommonOkDialog
 
 # Coding Level 1
 
+@sims4.commands.Command('level1:ok1', command_type=sims4.commands.CommandType.Live)
+def _ok(_connection: int = None):
+    output = sims4.commands.CheatOutput(_connection)
+    output('Showing test ok dialog.')
+
+    def _on_acknowledged(_dialog: UiDialogOk):
+        if _dialog.accepted:
+            output('Ok option chosen.')
+        else:
+            output('Dialog closed.')
+
+    try:
+        # LocalizedStrings within other LocalizedStrings
+        title_tokens = (CommonLocalizationUtils.create_localized_string('Story Headline', text_color=CommonLocalizedStringColor.GREEN),)
+        description_tokens = (CommonLocalizationUtils.create_localized_string('Actual Story', tokens=(CommonSimUtils.get_active_sim_info(),), text_color=CommonLocalizedStringColor.BLUE),)
+        dialog = CommonOkDialog(
+            'This is you!', # this is where the headline is written -> insert headline here
+            'You wanted to be a computer scientist your whole life and now, finally, that dream is coming true! '
+            'You just received the Letter – you are an employee at Wayfair starting Monday morning. '
+            'But let’s be honest you sent too many letters of application to even remember what kind of company Wayfair'
+            ' is, right? Let’s check it out!', # this is where the body part is written -> insert tet here
+            title_tokens=title_tokens,
+            description_tokens=description_tokens,
+            ok_text_identifier=CommonLocalizationUtils.create_localized_string('Lets go!', text_color=CommonLocalizedStringColor.RED)
+        )
+        dialog.show(on_acknowledged=_on_acknowledged)
+    except Exception as ex:
+        CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to show dialog', exception=ex)
+        output('Failed to show ok dialog, please locate your exception log file.')
+    output('Done showing.')
+
+
+@sims4.commands.Command('level1_ok2', command_type=sims4.commands.CommandType.Live)
+def _ok(_connection: int = None):
+    output = sims4.commands.CheatOutput(_connection)
+    output('Showing test ok dialog.')
+
+    def _on_acknowledged(_dialog: UiDialogOk):
+        if _dialog.accepted:
+            output('Ok option chosen.')
+        else:
+            output('Dialog closed.')
+
+    try:
+        # LocalizedStrings within other LocalizedStrings
+        title_tokens = (CommonLocalizationUtils.create_localized_string('Story Headline', text_color=CommonLocalizedStringColor.GREEN),)
+        description_tokens = (CommonLocalizationUtils.create_localized_string('Actual Story', tokens=(CommonSimUtils.get_active_sim_info(),), text_color=CommonLocalizedStringColor.BLUE),)
+        dialog = CommonOkDialog(
+            'XXXXXX...',# this is where the headline is written -> insert headline here
+            'That really does sound familiar. '
+            'Where do I know that name from? '
+            'Wasn’t it like a childhood friend or something?',# this is where the body part is written -> insert tet here
+            title_tokens=title_tokens,
+            description_tokens=description_tokens,
+            ok_text_identifier=CommonLocalizationUtils.create_localized_string('Maybe... ', text_color=CommonLocalizedStringColor.RED)
+        )
+        dialog.show(on_acknowledged=_on_acknowledged)
+    except Exception as ex:
+        CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to show dialog', exception=ex)
+        output('Failed to show ok dialog, please locate your exception log file.')
+    output('Done showing.')
+
+
 @sims4.commands.Command('level1_ok3', command_type=sims4.commands.CommandType.Live)
 def _ok(_connection: int = None):
     output = sims4.commands.CheatOutput(_connection)
