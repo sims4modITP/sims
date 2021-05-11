c55ddcd86af2200d0d250bb39eb8019ca524de7a
kiubb02 <71399699+kiubb02@users.noreply.github.com>
2021-05-11 19:33:49 +0200

    Update level6.py

--- level/level6.py
+++ level/level6.py
@@ -1,1 +1,34 @@
 # functions for level 6
+
+
+# First notification 
+@sims4.commands.Command('notif1_level_6', command_type=sims4.commands.CommandType.Live)
+def common_testing_show_basic_notification(self):
+    try:
+        # LocalizedStrings within other LocalizedStrings
+        title_tokens = (
+            CommonLocalizationUtils.create_localized_string(
+                "string id",  # string id form string table
+                text_color=CommonLocalizedStringColor.BLUE
+            ),
+        )
+        description_tokens = (
+            CommonLocalizationUtils.create_localized_string(
+                "string id",
+                text_color=CommonLocalizedStringColor.BLUE
+            ),
+        )
+        dialog = CommonBasicNotification(
+            "FOUND SOMETHING",
+            "I think I have found something important ... ",
+            title_tokens=title_tokens,
+            description_tokens=description_tokens,
+        )
+        dialog.show(IconInfoData(CommonResourceUtils.get_resource_key(Types.PNG, 16900524886024814375)))
+    except Exception as ex:
+        CommonExceptionHandler.log_exception(ModInfo.get_identity(),
+                                             'Failed to show a basic notification you fucked up',
+                                             exception=ex)
+
+# Targeted Question Dialog aus Level 6
+
