13f19a6b59febba61cbe869cd2d348468b4e06bc
inaschmina <80634958+inaschmina@users.noreply.github.com>
2021-05-20 15:46:26 +0200

    Update input.py

--- INPUT-Dialog/input.py
+++ INPUT-Dialog/input.py
@@ -64,3 +64,52 @@ def _common_testing_show_input_float_dialog(_connection: int = None):
         CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to show dialog', exception=ex)
         output('Failed to show dialog, please locate your exception log file.')
     output('Done showing.')
+
+    
+# input with repeading pop-up as long as answer is incorrect
+@sims4.commands.Command('input_first_level_three', command_type=sims4.commands.CommandType.Live)
+def _common_testing_show_input_float_dialog(_connection: int = None):
+    output = sims4.commands.CheatOutput(_connection)
+    output('Showing test input float dialog.')
+
+    def _on_chosen(choice: float, outcome: CommonChoiceOutcome):
+        if choice == 2 or choice == 3 or choice == None: 
+            output("answer right or dialogue dismissed")
+        else:
+            output("answer wrong")
+            # show dialogue as oftem as needed recoursively while answer is wrong
+            dialog2.show(on_submit=_on_chosen)
+
+        output('Chose {} with result: {}.'.format(pformat(choice), pformat(outcome)))
+
+    try:
+        # LocalizedStrings within other LocalizedStrings
+        title_tokens = (CommonLocalizationUtils.create_localized_string
+                        ("You nearly did it!", text_color=CommonLocalizedStringColor.BLUE),)
+        description_tokens = (CommonLocalizationUtils.create_localized_string
+                              ("Your have nearly made it to the final destination of your work to reach the next hint.\nThere is one more thing you have to solve before you can take a look at it: \n\nWhat might be the Evil behind all that \n(1) Pop-Culture \n(2) Capitalism \n(3) Men ",
+                               tokens=(CommonSimUtils.get_active_sim_info(),),
+                               text_color=CommonLocalizedStringColor.BLUE),)
+        dialog = CommonInputFloatDialog(
+            CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
+            CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
+            0,
+            title_tokens=title_tokens,
+            description_tokens=description_tokens
+        )
+        # new title -> error message for false input
+        title_tokens2 = (CommonLocalizationUtils.create_localized_string
+                        ("Think again", text_color=CommonLocalizedStringColor.RED),)
+        # in new dialogue -> is to show when input wrong
+        dialog2 = CommonInputFloatDialog(
+            CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
+            CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
+            0,
+            title_tokens=title_tokens2,
+            description_tokens=description_tokens
+        )
+        dialog.show(on_submit=_on_chosen)
+    except Exception as ex:
+        CommonExceptionHandler.log_exception(ModInfo.get_identity(), 'Failed to show dialog', exception=ex)
+        output('Failed to show dialog, please locate your exception log file.')
+    output('Done showing.')
